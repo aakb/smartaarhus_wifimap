d817dfccc71333bac6c7aa631bd57f5f6fbf9695
Brian Jensen <hallo@magnify.dk>
2014-01-20 16:39:24 +0100

    Removed margin bottom variable and added IE 10 password field override

--- scripts/common.js
+++ scripts/common.js
@@ -6,10 +6,14 @@
 
 
 $(document).ready(function() {
-  // Attach toggle password function
+  // Attach toggle password function.
+  // URL: https://github.com/cloudfour/hideShowPassword
+
   $('.js-form-toggle-password').click(function() {
+
     // Get element from data attribute and attach togglePassword function.
     $('#' + $(this).attr('data-toggle-password'))
     .togglePassword();
+
   });
 });
